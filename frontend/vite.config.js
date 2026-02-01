0c8b1c164030e3bd08794662b0b0b4d4d2582f87
samiran <sam.de.721166@gmail.com>
2026-02-01 14:21:16 +0530

    d

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: process.env.BACKEND_URL || "http://localhost:3000",
+        target: process.env.BACKEND_URL,
         changeOrigin: true,
       },
     },
